449f006f5ea95457e71cbe803b22049d45406332
aleksandrov2 <jozef.franc@yahoo.com>
2016-01-13 23:49:52 +0100

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -20,7 +20,7 @@ stran2 <- html_session(link2) %>% read_html()
 stran2 <- html_session(link2) %>% read_html()
 tabela2 <- stran2 %>%
   html_nodes(xpath="//table[@class='table table-condensed table-hover']") %>%
-  .[[1]] %>% html_table()
+  .[[1]] %>% html_table()  #problem je da je to prva tabela in ne druga tabela
 
 
 #link do wikipedije kjer sem dobil podatke
