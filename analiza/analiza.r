6181f3aedd2e77251f1ad8b94fd18106c2012e28
aleksandrov2 <jozef.franc@yahoo.com>
2016-01-15 20:18:43 +0100

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -13,17 +13,14 @@ podatki3_2014 <- podatki3 %>% filter(Cas == 2014)
 rownames(podatki3_2014) <- podatki3_2014$Drzava
 podatki3_2014 <- podatki3_2014[c("Dolg", "Deficit")]
 podatki3.norm <- scale(podatki3_2014)
-k <- kmeans(podatki3.norm, 5)
-table(k$cluster)
-
 
 podatki3.skupine <- data.frame(Drzava = names(k$cluster), 
                                skupina = factor(k$cluster))
 
+#prvi zemljevid kaže razdelitev evropskih držav na dve skupini
+k <- kmeans(podatki3.norm, 2)
+k <- kmeans(podatki3.norm, 2, nstart = 10000)
 
-k$tot.withinss
-k <- kmeans(podatki3.norm, 10, nstart = 10000)
-k$tot.withinss
 
 skupina <- podatki3.skupine
 m3 <- match(svet$adm0_a3, skupina$Drzava)
@@ -37,5 +34,26 @@ zem3 <- ggplot() + geom_polygon(data = evropa, aes(x=long, y=lat,
   xlab("") + ylab("") 
 
 
-
 #plot(zem3)
+
+
+#Ta graf prikazuje razdelitev evrope na 5 skupin držav
+
+k2 <- kmeans(podatki3.norm, 5, nstart = 10000)
+podatki3.skupine2 <- data.frame(Drzava = names(k2$cluster), 
+                               skupina = factor(k2$cluster))
+
+
+skupina2 <- podatki3.skupine2
+m4 <- match(svet$adm0_a3, skupina2$Drzava)
+svet$skupina2 <- skupina2$skupina2[m4]
+evropa <- pretvori.zemljevid(svet, svet$continent == "Europe")
+zem4 <- ggplot() + geom_polygon(data = evropa, aes(x=long, y=lat, 
+                                                   group = group, fill = skupina2),
+                                color = "grey") + xlim(-10, 50) + 
+  ylim(34, 72) + 
+  #scale_fill_continuous(low = "#69b8f6", high = "#142d45") + 
+  xlab("") + ylab("") 
+
+
+#plot(zem4)
