d15bdfd9c8d7fbc109c83ff30cb833fa183a1745
aleksandrov2 <jozef.franc@yahoo.com>
2016-01-16 15:23:04 +0100

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,22 +1,21 @@
 # 4. faza: Analiza podatkov
 
-# 4. faza: Analiza podatkov
-
 napoved <- lm(data = podatki3 %>% filter(Cas == 2006), Deficit ~ Dolg)
 predict(napoved, data.frame(Dolg=seq(0, 250, 25))) 
 
 napoved2 <- lm(data = podatki3 %>% filter(Cas == 2014), Deficit ~ Dolg)
 predict(napoved2, data.frame(Dolg=seq(0, 250, 25)))
 
-Zadolzenost <- c(0,25,50,75,100,125,150,175,200,225,250)
-Deficit <- c(0.80,-0.15,-1.09,-2.03,-2.98,-3.92,-4.86,-5.81,-6.75,-7.69,-8.63)
 
+napoved.tabela <- data.frame(Dolg=seq(0, 250, 25))
+napoved.tabela$Deficit <- predict(napoved, napoved.tabela) 
 
-#rad bi naredil tabelo, ki bi prikazovala napoved gibanja deficita, če vemo
-#kakšna je naša zadolženost
+#View(napoved.tabela2)
 
-predikcija <- as.table(setNames(Zadolzenost, Deficit))
+napoved.tabela2 <- data.frame(Dolg=seq(0, 250, 25))
+napoved.tabela2$Deficit <- predict(napoved2, napoved.tabela2) 
 
+#View(napoved.tabela2)
 
 
 #sedaj bi radi ločili države v skupine, glede na dolg in deficit
@@ -26,25 +25,26 @@ rownames(podatki3_2014) <- podatki3_2014$Drzava
 podatki3_2014 <- podatki3_2014[c("Dolg", "Deficit")]
 podatki3.norm <- scale(podatki3_2014)
 
-podatki3.skupine <- data.frame(Drzava = names(k$cluster), 
-                               skupina = factor(k$cluster))
+
 
 #prvi zemljevid kaže razdelitev evropskih držav na dve skupini
 k <- kmeans(podatki3.norm, 2)
 table(k$cluster)
 k <- kmeans(podatki3.norm, 2, nstart = 10000)
 
+podatki3.skupine <- data.frame(Drzava = names(k$cluster), 
+                               skupina = factor(k$cluster))
 
-skupina <- podatki3.skupine
-m3 <- match(svet$adm0_a3, skupina$Drzava)
-svet$skupina <- skupina$skupina[m3]
+sever.jug <- skupina[c("SWE", "GRC"), "skupina"]
+svet$skupina <- factor(skupina$skupina[m3], levels = sever.jug, ordered = TRUE)
 evropa <- pretvori.zemljevid(svet, svet$continent == "Europe")
 zem3 <- ggplot() + geom_polygon(data = evropa, aes(x=long, y=lat, 
-                                group = group, fill = skupina),
+                                                   group = group, fill = skupina),
                                 color = "grey") + xlim(-10, 50) + 
-  ylim(34, 72) + 
-  #scale_fill_continuous(low = "#69b8f6", high = "#142d45") + 
-  xlab("") + ylab("") 
+  ylim(34, 72) + xlab("") + ylab("") +
+  scale_fill_manual(values = setNames(c("#00bfc4", "#f8766d"), sever.jug),
+                    labels = setNames(c("Sever", "Jug"), sever.jug),
+                    na.value = "#7f7f7f")
 
 #plot(zem3)
 
@@ -53,18 +53,20 @@ zem3 <- ggplot() + geom_polygon(data = evropa, aes(x=long, y=lat,
 
 k2 <- kmeans(podatki3.norm, 5, nstart = 10000)
 podatki3.skupine2 <- data.frame(Drzava = names(k2$cluster), 
-                               skupina = factor(k2$cluster))
+                                skupina = factor(k2$cluster))
 
 
 skupina2 <- podatki3.skupine2
 m4 <- match(svet$adm0_a3, skupina2$Drzava)
-svet$skupina2 <- skupina2$skupina2[m4]
+svet$skupina2 <- skupina2$skupina[m4]
 evropa <- pretvori.zemljevid(svet, svet$continent == "Europe")
 zem4 <- ggplot() + geom_polygon(data = evropa, aes(x=long, y=lat, 
                                                    group = group, fill = skupina2),
                                 color = "grey") + xlim(-10, 50) + 
   ylim(34, 72) + 
-  #scale_fill_continuous(low = "#69b8f6", high = "#142d45") + 
   xlab("") + ylab("") 
 
 #plot(zem4)
+
+
+
