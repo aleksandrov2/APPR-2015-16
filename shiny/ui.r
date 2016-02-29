97197ec0b006b788e92ab4494c005346fa252af8
aleksandrov2 <jozef.franc@yahoo.com>
2016-02-29 23:29:57 +0100

    Update ui.r

--- shiny/ui.r
+++ shiny/ui.r
@@ -1,16 +1,23 @@
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
-
-
 library(shiny)
-
-
-shinyUI(fluidPage(
+shinyUI(
+ui <- fluidPage(
   
-  plotOutput("prvi_graf"),
-  tableOutput("napoved.tabela"),
-  plotOutput("enajsti_graf")))
+  titlePanel("Analiza dolga in primankljaja držav v Evropski uniji"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId="leto_1",label="Leto",min=2006,max=2014,value=2007,step=1),
+      sliderInput(inputId="leto_2",label="Leto",min=2006,max=2014,value=2007,step=1),
+      sliderInput(inputId="leto_3",label="Leto",min=2006,max=2014,value=2007,step=1)
+    ),
+    mainPanel(
+      tabsetPanel(tabPanel("Comparison",dataTableOutput("table"),plotOutput("plot"),
+                           splitLayout(
+                             plotOutput("age"),
+                             plotOutput("origin"),
+                             plotOutput("sex"))),
+                  tabPanel("Prediction",plotOutput("prediction"))
+      )
+    )
+  )
+)
+)
