c11a42f00bf48aaace737eebc30f682dfe9af2a4
aleksandrov2 <jozef.franc@yahoo.com>
2016-02-29 23:57:28 +0100

    Update server.r

--- shiny/server.r
+++ shiny/server.r
@@ -21,13 +21,6 @@ server <- function(input, output) {
   ylim(-33, 6)})
   
   
-  output$zemljevid <- renderPlot({
-  Dolg <- filter(podatki1, Cas == input$leto_3)
-  m1 <- match(svet$adm0_a3, Dolg$Drzava)
-  svet$Dolg <- Dolg$Dolg[m1]
-  evropa <- pretvori.zemljevid(svet, svet$continent == "Europe")
-  ggplot() + geom_polygon(data = evropa, aes(x=long, y=lat, group = group, fill = Dolg), color = "grey") + 
-  xlim(-10, 50) + ylim(34, 72) + scale_fill_continuous(low = "#69b8f6", high = "#142d45",limits = c(0, 200)) + 
-  xlab("") + ylab("") + ggtitle("Zemljevid zadolženosti evropskih držav")})
+  
 }
 )
