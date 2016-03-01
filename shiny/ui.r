40fc45a0d0df62fb438ce41a666516b430b5564d
aleksandrov2 <jozef.franc@yahoo.com>
2016-03-01 18:02:17 +0100

    Update ui.r

--- shiny/ui.r
+++ shiny/ui.r
@@ -3,20 +3,19 @@ shinyUI(
   ui <- fluidPage(
     
     titlePanel("Analiza dolga in primankljaja držav v Evropski uniji"),
-    sidebarLayout(
-      sidebarPanel(
-        sliderInput(inputId="leto_1",label="Leto",min=2006,max=2014,value=2007,sep=""),
-        sliderInput(inputId="leto_2",label="Leto",min=2006,max=2014,value=2007,sep="")
-        
-      ),
-      mainPanel(
-        tabsetPanel(tabPanel("Grafi",dataTableOutput("table"),plotOutput("plot"),
-                             splitLayout(
-                               plotOutput("dolg"),
-                               plotOutput("deficit")
-                               ))
-        )
-      )
+    tabsetPanel(
+      tabPanel("Dolg",
+               sliderInput(inputId="leto_1",label="Leto",min=2006,max=2014,value=2007,sep=""),
+               plotOutput("dolg")),
+      tabPanel("Deficit",
+               sliderInput(inputId="leto_2",label="Leto",min=2006,max=2014,value=2007,sep=""),
+               plotOutput("deficit")),
+      tabPanel("Zadolženost",
+               radioButtons("variable","Drzave:",inline=TRUE,choices = c("SVN",'AUT',"BEL","CZE","DEU","DNK","ESP","EST","FIN","FRA","GBR","GRC","HUN","IRL","ITA","LUX","NLD","POL","PRT","SVK","SWE")),
+               plotOutput("zadolženost")),
+      tabPanel("Zemljevid",
+               sliderInput(inputId="leto_3",label="Leto",min=2006,max=2014,value=2006,sep=""),
+               plotOutput("zemljevid"))
     )
   )
 )
